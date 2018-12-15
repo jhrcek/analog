3fc1b3d41cba2d9c29859ee55981661092b996e7
Jan Hrcek <jhrcek@redhat.com>
2018-12-15 09:01:31 +0100

    Bump to lts-12.22, code style improvements

--- Main.hs
+++ Main.hs
@@ -4,17 +4,20 @@ that are followed by too large a delay.
 Usage: analog <delay-in-seconds> </path/to/framework.log>
 -}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 module Main where
-import           Data.Char          (isDigit)
-import           Data.Function      (on)
-import           Data.Monoid        ((<>))
-import           Data.Text          (Text)
-import qualified Data.Text          as T
-import qualified Data.Text.IO       as IO
-import           Data.Time          (NominalDiffTime, UTCTime)
-import           Data.Time.Clock    (diffUTCTime)
-import           Data.Time.Format   (defaultTimeLocale, parseTimeOrError)
-import           System.Environment (getArgs)
+
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text
+
+import Data.Function (on)
+import Data.Monoid ((<>))
+import Data.Text (Text)
+import Data.Time (NominalDiffTime, UTCTime)
+import Data.Time.Clock (diffUTCTime)
+import Data.Time.Format (defaultTimeLocale, parseTimeOrError)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 type Line = Text
 
@@ -22,20 +25,26 @@ main :: IO ()
 main = do
    args <- getArgs
    case args of
-       [delay, logfile] | all isDigit delay -> analyzeLog (read delay) logfile
-       _                                    -> putStrLn "Usage: analog <delay-in-seconds> </path/to/framework.log>"
+       [delayStr, logfile] | Just delay <- readMaybe @Int delayStr
+           -> analyzeLog delay logfile
+       _   -> putStrLn "Usage: analog <delay-in-seconds> </path/to/framework.log>"
 
 analyzeLog :: Int -> FilePath -> IO ()
-analyzeLog delay f = do
-    logData <- IO.readFile f
-    IO.putStrLn . T.unlines . linesWithDelayGreaterThan delay . calculateDelays . filter hasTime $ T.lines logData
+analyzeLog delay f =
+    Text.putStrLn
+    . Text.unlines
+    . linesWithDelayGreaterThan delay
+    . calculateDelays
+    . filter hasTime
+    . Text.lines
+    =<< Text.readFile f
 
 hasTime :: Line -> Bool
-hasTime l = T.isPrefixOf "[" l && T.isInfixOf "] " l
+hasTime l = Text.isPrefixOf "[" l && Text.isInfixOf "]" l
 
 linesWithDelayGreaterThan :: Int -> [(NominalDiffTime, Line)] -> [Line]
 linesWithDelayGreaterThan minDelay =
-    map (\(d,l)-> "delay " <> T.pack (show d) <> ": " <> l)
+    map (\(d,l)-> "delay " <> Text.pack (show d) <> ": " <> l)
     . filter (\(d,_) -> d > fromIntegral minDelay)
 
 -- For each line in the list calculates time it took for the following line to appear
@@ -51,6 +60,6 @@ calculateDelays ls =
 extractTime :: Line -> UTCTime
 extractTime =
     parseTimeOrError True defaultTimeLocale "%F %T,%q"
-    . T.unpack
+    . Text.unpack
     . (<> "000000000") --apend 9 zeroes & parse as nanoseconds
-    . T.takeWhile (/=']') . T.tail --extract part between '[' ']'
+    . Text.takeWhile (/=']') . Text.tail --extract part between '[' ']'
